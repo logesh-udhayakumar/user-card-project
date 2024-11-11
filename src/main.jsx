ea3044ab76da085b686e480a42055af512e65017
logesh <logesh.udhayakumar@farmsio.com>
2024-11-11 21:20:20 +0530

    workflow file added v6

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './UserCard.css'
-import { UserCard } from './usercard'
+import { UserCard } from './UserCard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UserCard />
+    <UserCard/>
   </StrictMode>,
 )
